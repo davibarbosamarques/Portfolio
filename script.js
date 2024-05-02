803228faad1fa56fece4149eb4b35c42114f346d
davibarbosamarques <dbarbosamarques100@gmail.com>
2024-05-02 00:23:59 -0300

    adicionei modo dark

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function menuShow() {
         document.querySelector('.icon').innerHTML = '<i class="bi bi-x-lg" style="font-size: 3.5vh;"></i>'
     }
 }
+
+// MODO LIGHT/DARK
+const btnmodedark = document.getElementById("btnModeDark");
+const themeSystem = localStorage.getItem("themeSystem") || "light"
+
+
+btnmodedark.addEventListener('click', () => {
+    let antigoTema = localStorage.getItem("themeSystem") || "light";
+    let novoTema = antigoTema == "light" ? "dark" : "light";
+
+    localStorage.setItem("themeSystem",novoTema)
+    definirtema(novoTema)
+})
+
+function definirtema(tema){
+    const icondark = '<i class="bi bi-moon-stars"></i>'
+    const iconlight = '<i class="bi bi-brightness-high"></i>'
+    document.documentElement.setAttribute("data-thema", tema)
+
+    if (tema == 'light'){
+        btnmodedark.innerHTML = icondark
+    }else{
+        btnmodedark.innerHTML = iconlight
+    }
+}
+
+definirtema(themeSystem)
